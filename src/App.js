cf2fa4fbdabadb0d1b736289d758ec725bec709a
Darryl <darrylmangibin@gmail.com>
2019-05-28 18:04:00 +0800

    wrap up authentication on firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import keys from '../config/keys';
 import LoginForm from './components/LoginForm';
 
 class App extends React.Component {
+
+  state = {
+    loggedIn: null
+  }
+
   componentWillMount() {
     firebase.initializeApp(keys);
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  renderContent = () => {
+    switch(this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button
+              onPress={() => firebase.auth().signOut()}
+            >
+              Log Out
+            </Button>
+          </CardSection>
+        )
+      case false:
+        return <LoginForm />
+      default:
+        return (
+          <CardSection>
+            <Spinner size='large' />
+          </CardSection>
+        ) 
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
